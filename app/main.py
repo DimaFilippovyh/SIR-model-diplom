1589b535775a6291becb3ad3b34dca246ef3a91d
DimaFilippovyh <diman.filippovyh@gmail.com>
2022-05-15 22:08:52 +0300

    Change main formulas, change flake8 rule(max length), add func witch koef two_wave

--- app/main.py
+++ app/main.py
@@ -1,25 +1,32 @@
 import sys
 from spread_virus_in_country import (
     spread_virus_in_country,
-    spread_virus_in_countries)
+    spread_virus_in_bifurcation,
+    spread_virus_in_countries
+)
 from Country import Country
 from parameters_contacts import get_interaction_parameters
 from constans import NAMES_OF_COUNTRIES
 
 
 def main():
-    number_situation = int(sys.argv[1])
+    try:
+        number_situation = int(sys.argv[1])
+    except IndexError:
+        print("Надо добавить параметр number_situation\n")
+        number_situation = -1
+    # number_situation = 4
 
     if number_situation == 0:
-        # для одной страны % done
+        # для одной страны
         country = Country('Russia', 0.03, 0.005)
         country.start_viruses()
         country_2 = Country('Russia', 0.03, 0.005)
         country_2.start_viruses()
         spread_virus_in_country(country, country_2)
-    else:
-        parameters_contact_countries = get_interaction_parameters(
-            number_situation)
+
+    elif number_situation in (1, 2):
+        parameters_contact_countries = get_interaction_parameters(number_situation)
 
         lst_countries = []
         for name in NAMES_OF_COUNTRIES:
@@ -29,7 +36,7 @@ def main():
         lst_countries_with_contacts = []
         for name in NAMES_OF_COUNTRIES:
             country = Country(name, *parameters_contact_countries[0].get(name),
-                parameters_contact_countries[1].get(name))
+                              parameters_contact_countries[1].get(name))
             lst_countries_with_contacts.append(country)
 
         if number_situation == 1:
@@ -46,6 +53,10 @@ def main():
             lst_countries_with_contacts[0].start_viruses()
 
         spread_virus_in_countries(lst_countries, lst_countries_with_contacts)
+
+    elif number_situation == 3:
+        """Ситуация для бифуркации"""
+        spread_virus_in_bifurcation()
 
 
 if __name__ == "__main__":
--- app/proliferation_virus.py
+++ app/proliferation_virus.py
@@ -14,26 +14,21 @@ def proliferation_virus_for_one_country_without_two_wave(country: Country):
     k_two_wave - коэф. с которым люди заболевают повторно
     """
     name = country.name
-    infected_people, none_infected_people, recovered_people = \
-        country.get_situation_in_country()
+    infected_people, none_infected_people, recovered_people = country.get_situation_in_country()
     contacting, recovered = country.get_parameters_country()
 
-    tmp_infected_people = contacting * infected_people[-1] * \
-        none_infected_people[-1]
+    tmp_infected_people = contacting * infected_people[-1] * none_infected_people[-1]
+    tmp_recovered_people = recovered * infected_people[-1 - length_virus]
 
-    tmp_recovered_people = recovered * \
-        infected_people[-1 - length_virus]
-
-    tomr_infected_people = tmp_infected_people - tmp_recovered_people + \
-        infected_people[-1]
-
-    tomr_none_infected_people = none_infected_people[-1] - \
-        tmp_infected_people
-
+    tomr_infected_people = tmp_infected_people - tmp_recovered_people + infected_people[-1]
+    tomr_none_infected_people = none_infected_people[-1] - tmp_infected_people
     tomr_recovered_people = recovered_people[-1] + tmp_recovered_people
 
-    new_values = Country.normalize_solution([tomr_infected_people,
-        tomr_none_infected_people, tomr_recovered_people])
+    if (summ := tomr_infected_people + tomr_none_infected_people + tomr_recovered_people) > 1.1:
+        raise Exception(f"Сумма превысила 1 = {summ}")
+
+    # new_values = Country.normalize_solution([tomr_infected_people, tomr_none_infected_people, tomr_recovered_people])
+    new_values = [tomr_infected_people, tomr_none_infected_people, tomr_recovered_people]
 
     country.change_situation(new_values)
 
@@ -59,43 +54,39 @@ def proliferation_virus_for_one_country(country: Country):
     k_two_wave - коэф. с которым люди заболевают повторно
     """
 
-    k_two_wave = 0.1
+    k_two_wave = 0.5
     """
-    # при 0.1 побеждается болезнь
-    # при параметре 0.5 останавливаеться на 66 и 33 процентах
-    # при 0.7 гуляет в нижней части
-    # при 0.9 интересно по всему графику ходит
-    # при 1.1 побеждает болезнь
+    при 0.1 побеждается болезнь
+    при параметре 0.5 останавливаеться на 66 и 33 процентах
+    при 0.7 гуляет в нижней части
+    при 0.9 интересно по всему графику ходит
+    при 1.1 побеждает болезнь
     """
 
     name = country.name
-    infected_people, none_infected_people, recovered_people = \
-        country.get_situation_in_country()
+    infected_people, none_infected_people, recovered_people = country.get_situation_in_country()
     contacting, recovered = country.get_parameters_country()
 
-    tmp_infected_people = contacting * infected_people[-1] * \
-        none_infected_people[-1]
+    tmp_infected_people = contacting * infected_people[-1] * none_infected_people[-1]
+    tmp_recovered_people = recovered * infected_people[-1 - length_virus]
 
-    tmp_recovered_people = recovered * \
-        infected_people[-1 - length_virus]
-
-    if len(infected_people) > 92:
-        tmp_two_wave_infected_people = k_two_wave * contacting * \
-            infected_people[-1] * recovered_people[-1 - 90]
+    if len(recovered_people) > 92:
+        tmp_two_wave_recovered_people = k_two_wave * (recovered_people[-1 - 90] - recovered_people[-2 - 90])
+        # tmp_two_wave_recovered_people = k_two_wave * recovered_people[-1 - 90]
     else:
-        tmp_two_wave_infected_people = 0
+        tmp_two_wave_recovered_people = 0
 
-    tomr_infected_people = tmp_infected_people - tmp_recovered_people + \
-        infected_people[-1] + tmp_two_wave_infected_people  # FIXME:
+    tomr_infected_people = infected_people[-1] - tmp_recovered_people + tmp_infected_people
+    tomr_none_infected_people = none_infected_people[-1] - tmp_infected_people + tmp_two_wave_recovered_people
+    tomr_recovered_people = recovered_people[-1] + tmp_recovered_people - tmp_two_wave_recovered_people
 
-    tomr_none_infected_people = none_infected_people[-1] - \
-        tmp_infected_people
+    if (summ := tomr_infected_people + tomr_none_infected_people + tomr_recovered_people) > 1.1:
+        raise Exception(f"Сумма превысила 1 = {summ}")
 
-    tomr_recovered_people = recovered_people[-1] + tmp_recovered_people - \
-        tmp_two_wave_infected_people
+    # new_values = Country.normalize_solution([tomr_infected_people, tomr_none_infected_people, tomr_recovered_people])
 
-    new_values = Country.normalize_solution([tomr_infected_people,
-        tomr_none_infected_people, tomr_recovered_people])
+    new_values = [tomr_infected_people, tomr_none_infected_people, tomr_recovered_people]
+    print(new_values)
 
     country.change_situation(new_values)
 
@@ -106,6 +97,63 @@ def proliferation_virus_for_one_country(country: Country):
 
     if tomr_recovered_people > 0.98:
         print(F"All people recuperation in {name}!!!")
+    time.sleep(0)
+    # time.sleep(0.3)
+
+
+def proliferation_virus_with_two_wave_koef(country: Country, k_two_wave):
+    """
+    contacting - частота встречи людей
+    recovered - коэф. выздоровления
+    length_vir - протяженность болезни
+    infected_people - больные люди
+    none_infected_people = не болевшие люди
+    recovered_people - выздоровевшие
+    k_two_wave - коэф. с которым люди заболевают повторно
+    """
+
+    # k_two_wave = 0.0
+    """
+    # при 0.1 побеждается болезнь
+    # при параметре 0.5 останавливаеться на 66 и 33 процентах
+    # при 0.7 гуляет в нижней части
+    # при 0.9 интересно по всему графику ходит
+    # при 1.1 побеждает болезнь
+    """
+
+    name = country.name
+    infected_people, none_infected_people, recovered_people = country.get_situation_in_country()
+    contacting, recovered = country.get_parameters_country()
+
+    tmp_infected_people = contacting * infected_people[-1] * none_infected_people[-1]
+    tmp_recovered_people = recovered * infected_people[-1 - length_virus]
+
+    if len(recovered_people) > 92:
+        tmp_two_wave_recovered_people = k_two_wave * (recovered_people[-1 - 90] - recovered_people[-2 - 90])
+        # tmp_two_wave_recovered_people = k_two_wave * recovered_people[-1 - 90]
+    else:
+        tmp_two_wave_recovered_people = 0
+
+    tomr_infected_people = infected_people[-1] - tmp_recovered_people + tmp_infected_people
+    tomr_none_infected_people = none_infected_people[-1] - tmp_infected_people + tmp_two_wave_recovered_people
+    tomr_recovered_people = recovered_people[-1] + tmp_recovered_people - tmp_two_wave_recovered_people
+
+    if (summ := tomr_infected_people + tomr_none_infected_people + tomr_recovered_people) > 1.1:
+        raise Exception(f"Сумма превысила 1 = {summ}")
+
+    # new_values = Country.normalize_solution([tomr_infected_people, tomr_none_infected_people, tomr_recovered_people])
+
+    new_values = [tomr_infected_people, tomr_none_infected_people, tomr_recovered_people]
+
+    country.change_situation(new_values)
+
+    # print(name + "_V2", *new_values)
+    #
+    # if tomr_none_infected_people < 0.02:
+    #     print(F"All people infected in {name}")
+    #
+    # if tomr_recovered_people > 0.98:
+    #     print(F"All people recuperation in {name}!!!")
     time.sleep(0)
     # time.sleep(0.3)
 
@@ -121,26 +169,22 @@ def proliferation_virus(country: Country):
     k_two_wave - коэф. с которым люди заболевают повторно
     """
     name = country.name
-    infected_people, none_infected_people, recovered_people = \
-        country.get_situation_in_country()
+    infected_people, none_infected_people, recovered_people = country.get_situation_in_country()
     contacting, recovered = country.get_parameters_country()
 
-    tmp_infected_people = contacting * infected_people[-1] * \
-        none_infected_people[-1]
+    tmp_infected_people = contacting * infected_people[-1] * none_infected_people[-1]
+    tmp_recovered_people = recovered * infected_people[-1 - length_virus]
 
-    tmp_recovered_people = recovered * \
-        infected_people[-1 - length_virus]
-
-    tomr_infected_people = tmp_infected_people - tmp_recovered_people + \
-        infected_people[-1]
-
-    tomr_none_infected_people = none_infected_people[-1] - \
-        tmp_infected_people
-
+    tomr_infected_people = infected_people[-1] - tmp_recovered_people + tmp_infected_people
+    tomr_none_infected_people = none_infected_people[-1] - tmp_infected_people
     tomr_recovered_people = recovered_people[-1] + tmp_recovered_people
 
-    new_values = Country.normalize_solution([tomr_infected_people,
-        tomr_none_infected_people, tomr_recovered_people])
+    if (summ := tomr_infected_people + tomr_none_infected_people + tomr_recovered_people) > 1.1:
+        print(tomr_infected_people, tomr_none_infected_people, tomr_recovered_people)
+        raise Exception(f"Сумма превысила 1 = {summ}")
+
+    # new_values = Country.normalize_solution([tomr_infected_people, tomr_none_infected_people, tomr_recovered_people])
+    new_values = [tomr_infected_people, tomr_none_infected_people, tomr_recovered_people]
 
     country.change_situation(new_values)
 
@@ -168,42 +212,36 @@ def proliferation_virus_with_other_county(country: Country,
     other_infected_people - больные люди прибывшие из других стран
     """
     name = country.name
-    infected_people, none_infected_people, recovered_people = \
-        country.get_situation_in_country()
+    infected_people, none_infected_people, recovered_people = country.get_situation_in_country()
     contacting, recovered = country.get_parameters_country()
 
     other_names_of_countries = [i for i in NAMES_OF_COUNTRIES if i != name]
 
-    infected_people_from_other_countries = [
-        infected_people_in_countries.get(i) *
-        country.contacts_country.get(i)
+    infected_people_from_other_countries = [infected_people_in_countries.get(i) * country.contacts_country.get(i)
         for i in other_names_of_countries]
 
-    other_infected_people = [none_infected_people[-1] * i
-        for i in infected_people_from_other_countries]
+    other_infected_people = [none_infected_people[-1] * i for i in infected_people_from_other_countries]
 
-    tmp_infected_people = contacting * infected_people[-1] * \
-        none_infected_people[-1]
+    tmp_infected_people = contacting * infected_people[-1] * none_infected_people[-1]
 
-    # FIXME: chenge expression
+    # FIXME: change expression
     if tmp_infected_people == 0:
         tmp_infected_people = sum(other_infected_people)
     else:
-        tmp_infected_people *= (1 + sum(other_infected_people))
+        tmp_infected_people += sum(other_infected_people)
 
-    tmp_recovered_people = recovered * \
-        infected_people[-1 - length_virus]
+    tmp_recovered_people = recovered * infected_people[-1 - length_virus]
 
-    tomr_infected_people = tmp_infected_people - tmp_recovered_people + \
-        infected_people[-1]
-
-    tomr_none_infected_people = none_infected_people[-1] - \
-        tmp_infected_people
-
+    tomr_infected_people = infected_people[-1] - tmp_recovered_people + tmp_infected_people
+    tomr_none_infected_people = none_infected_people[-1] - tmp_infected_people
     tomr_recovered_people = recovered_people[-1] + tmp_recovered_people
 
-    new_values = Country.normalize_solution([tomr_infected_people,
-        tomr_none_infected_people, tomr_recovered_people])
+    if (summ := tomr_infected_people + tomr_none_infected_people + tomr_recovered_people) > 1.1:
+        print(tomr_infected_people, tomr_none_infected_people, tomr_recovered_people)
+        raise Exception(f"Сумма превысила 1 = {summ}")
+
+    # new_values = Country.normalize_solution([tomr_infected_people, tomr_none_infected_people, tomr_recovered_people])
+    new_values = [tomr_infected_people, tomr_none_infected_people, tomr_recovered_people]
 
     country.change_situation(new_values)
 
@@ -232,55 +270,47 @@ def proliferation_virus_with_other_county_with_two_wave(country: Country,
     """
     # FIXME:
 
-    k_two_wave = 1.1
+    k_two_wave = 0.9
     """
     при 0.5 переливы
     """
 
     name = country.name
-    infected_people, none_infected_people, recovered_people = \
-        country.get_situation_in_country()
+    infected_people, none_infected_people, recovered_people = country.get_situation_in_country()
     contacting, recovered = country.get_parameters_country()
 
     other_names_of_countries = [i for i in NAMES_OF_COUNTRIES if i != name]
 
-    infected_people_from_other_countries = [
-        infected_people_in_countries.get(i) *
-        country.contacts_country.get(i)
+    infected_people_from_other_countries = [infected_people_in_countries.get(i) * country.contacts_country.get(i)
         for i in other_names_of_countries]
 
-    other_infected_people = [none_infected_people[-1] * i
-        for i in infected_people_from_other_countries]
+    other_infected_people = [none_infected_people[-1] * i for i in infected_people_from_other_countries]
 
-    tmp_infected_people = contacting * infected_people[-1] * \
-        none_infected_people[-1]
+    tmp_infected_people = contacting * infected_people[-1] * none_infected_people[-1]
+    tmp_recovered_people = recovered * infected_people[-1 - length_virus]
 
     # FIXME: chenge expression
     if tmp_infected_people == 0:
         tmp_infected_people = sum(other_infected_people)
     else:
-        tmp_infected_people *= (1 + sum(other_infected_people))
+        tmp_infected_people += sum(other_infected_people)
 
-    tmp_recovered_people = recovered * \
-        infected_people[-1 - length_virus]
-
-    if len(infected_people) > 92:
-        tmp_two_wave_infected_people = k_two_wave * contacting * \
-            infected_people[-1] * recovered_people[-1 - 90]
+    if len(recovered_people) > 92:
+        tmp_two_wave_recovered_people = k_two_wave * (recovered_people[-1 - 90] - recovered_people[-2 - 90])
+        # tmp_two_wave_recovered_people = k_two_wave * recovered_people[-1 - 90]
     else:
-        tmp_two_wave_infected_people = 0
+        tmp_two_wave_recovered_people = 0
 
-    tomr_infected_people = tmp_infected_people - tmp_recovered_people + \
-        infected_people[-1] + tmp_two_wave_infected_people
+    tomr_infected_people = tmp_infected_people - tmp_recovered_people + infected_people[-1]
+    tomr_none_infected_people = none_infected_people[-1] - tmp_infected_people + tmp_two_wave_recovered_people
+    tomr_recovered_people = recovered_people[-1] + tmp_recovered_people - tmp_two_wave_recovered_people
 
-    tomr_none_infected_people = none_infected_people[-1] - \
-        tmp_infected_people
+    if (summ := tomr_infected_people + tomr_none_infected_people + tomr_recovered_people) > 1.1:
+        print(tomr_infected_people, tomr_none_infected_people, tomr_recovered_people)
+        raise Exception(f"Сумма превысила 1 = {summ}")
 
-    tomr_recovered_people = recovered_people[-1] + tmp_recovered_people - \
-        tmp_two_wave_infected_people
-
-    new_values = Country.normalize_solution([tomr_infected_people,
-        tomr_none_infected_people, tomr_recovered_people])
+    # new_values = Country.normalize_solution([tomr_infected_people, tomr_none_infected_people, tomr_recovered_people])
+    new_values = [tomr_infected_people, tomr_none_infected_people, tomr_recovered_people]
 
     country.change_situation(new_values)
 
